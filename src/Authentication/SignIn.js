7b690f721f5c0ab6c73008d5a82393791a9fb320
kennymanman <61712644+kennymanman@users.noreply.github.com>
2023-07-28 03:48:53 -1200

    Login Screen complete

--- src/Authentication/SignIn.js
+++ src/Authentication/SignIn.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { UserAuth } from '../context/AuthContext';
+import signinscreenone from "../images/signinscreenone.jpg"
 
 
 const Signin = () => {
@@ -19,8 +20,9 @@ const Signin = () => {
       await signIn(email, password)
       navigate('/Dashboard')
     } catch (e) {
-      setError(e.message)
+      setError( )
       console.log(e.message)
+      
     }
   };
 
@@ -33,6 +35,15 @@ const Signin = () => {
 
 <div>
 
+
+<img 
+className='object-bottom object-cover w-full h-full  absolute '
+src={signinscreenone} alt="newsletter"
+/>
+
+
+
+{/*
 <h1 className='text-2xl font-bold py-2'>Sign in to your account</h1>
 
 <p className='py-2'>
@@ -47,7 +58,51 @@ const Signin = () => {
           <input onChange={(e) => setPassword(e.target.value)} className='border p-3' type='password' />
 
 
-          <button onClick={handleSubmit}> Sign In</button>
+          <button onClick={handleSubmit}> Sign In</button> */}
+
+<div className='grid grid-cols-5 gap-2 '>
+
+<div className='col-span-2 relative p-3 '>
+
+
+<h2 className='text-white font-custom text-2xl font-medium tracking-tighter relative mt-2'>August AI.</h2>
+
+<hr className='relative border-white border-2'/>
+
+<div className='mt-80'>
+<h1 className='text-white font-custom text-7xl font-medium tracking-tighter '>Sign in to your account</h1>
+
+<h1 className='text-white font-custom text-3xl font-medium tracking-tighter mt-7'>Collaboration + AI + Chatbot.</h1>
+
+<h4 className='text-white font-custom text-sm font-medium tracking-tighter'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</h4>
+</div>
+
+
+</div>
+
+
+<div className='col-span-3  p-3  relative h-screen grid  place-items-end  '>
+
+
+<div className='flex flex-col gap-4 w-1/2   '>
+<input onChange={(e) => setEmail(e.target.value)} className='border-b-2 py-4 w-full relative bg-transparent rounded-t-lg placeholder:text-white p-2 text-white text-2xl' type='email' placeholder='EMAIL' />
+
+
+<input onChange={(e) => setPassword(e.target.value)} className='border-b-2 py-4 w-full relative bg-transparent rounded-t-lg placeholder:text-white p-2 text-white text-2xl' type='password' placeholder='PASSWORD' />
+
+<button onClick={handleSubmit} className='w-full bg-black text-white rounded-lg py-3 font-medium tracking-tighter font-custom text-xl  '>Log in</button>
+
+<h4 className='text-white font-custom text-sm font-medium tracking-tighter'>Dont have an account or forgot your password or email? send a message to the office admin for your login details or change of password/email.</h4>
+</div>
+  
+</div>
+
+
+
+</div>
+
+
 
 </div>
   );
